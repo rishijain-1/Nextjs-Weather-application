54714dc6a48d9bf974a6f2fbae4a00ee91afa579
rishijain2002 <rishijain0652@gmail.com>
2024-08-23 15:46:49 +0530

    Create seachbar Component

---
+++ src/components/Searchbox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoSearch } from 'react-icons/io5'
+
+type Props = {}
+
+export default function Searchbox({}: Props) {
+  return (
+    <form className="flex realtive items-center justify-center h-10">
+        <input type="text" placeholder="Search location.." className='px-4 py-2 w-[230px] border border-gray-300 rounded-1-md focus:outline-none
+        focus:border-blue-500 h-full'/>
+        <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md  focus:outline-none hover:bg-blue-600 whitespace-nowrap h-full">
+            <IoSearch/>
+        </button>
+
+    </form>
+  )
+}
